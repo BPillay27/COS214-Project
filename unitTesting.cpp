7e7f233c25b21fd78346338af9823797cc9c44e2
Byron Pillay <byronpillay2704@gmail.com>
2025-10-21 11:17:33 +0200

    Temporary unit testing.
    Temp unit testing for Commands, States and Order.

--- unitTesting.cpp
+++ unitTesting.cpp
@@ -1,1 +1,53 @@
+#include "Command.h"
+#include "Order.h"
+#include "OrderState.h"
+#include <iostream>
+#include <vector>
 
+int main()
+{
+    OrderState *a = new AcceptPayment(nullptr);
+    OrderState *d = new DeclinePayment(nullptr);
+    OrderState *p = new ProcessingPayment(nullptr);
+    OrderState *v = new VerifyOrder(nullptr);
+
+    a->handle();
+    d->handle();
+    p->handle();
+    v->handle();
+
+    /*
+    //These can be updated and tested once the Composite implementation is done.
+    //Order* order=new Order("20251021-01");
+    order->processPayment();
+    std::vector<AddToOrder*> addCommands;
+
+    OrderComponent* temp;
+    for(int i=0;i<5;i++){
+        temp=new OrderComponent();
+        AddToOrder* addCommand=new AddToOrder(order, temp);
+        addCommands.push_back(addCommand);
+    }
+
+    for(AddToOrder* a : addCommands){
+        a->execute();
+    }
+
+    RemoveFromOrder* rm=new RemoveFromOrder(order, temp);
+
+    order->getState();
+    order->getItems();
+    order->processPayment();
+
+    delete order;
+    delete rm;
+
+    for(AddToOrder* a : addCommands){
+        delete a;
+    }
+    */
+    delete a;
+    delete d;
+    delete p;
+    delete v;
+}
