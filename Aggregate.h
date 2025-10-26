9b919a984879b223c285e6606aac60c632a8481f
Byron Pillay <byronpillay2704@gmail.com>
2025-10-26 17:10:03 +0200

    Aggregate classes complete
    Aggregate classes complete, improvements to Iterator will follow

---
+++ Aggregate.h
@@ -0,0 +1,56 @@
+#ifndef AGGREGATE_H
+#define AGGREGATE_H
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Plant;
+class PlantRow;
+template <typename T>
+class Iterator;
+
+template <typename T>
+class Aggregate
+{
+public:
+    Aggregate() = default;
+    virtual ~Aggregate() = default;
+    virtual bool addPlant(Plant *toAdd) = 0;
+    virtual Iterator<T> *createIterator() = 0;
+};
+
+class PlantArea : public Aggregate<PlantRow*>
+{
+    public:
+    Iterator<PlantRow*>* createIterator() override;
+    bool addPlant(Plant* toAdd) override;
+    bool removePlant(Plant* toRemove);
+    PlantArea(int capacity);
+    ~PlantArea() override;
+    Plant* givePlant(std::string plantType);
+    int getCount();
+
+    private:
+    int capacity;
+    std::vector<PlantRow*> plantRows;
+    int count;
+};
+
+class PlantRow : public Aggregate<Plant *>
+{
+public:
+    PlantRow();
+    ~PlantRow() override;
+    int getNumPlants();
+    bool addPlant(Plant *toAdd) override;
+    bool removePlant(Plant *toRemove);
+    std::string typePlant();
+    Plant *givePlant();
+    Iterator<Plant *> *createIterator() override;
+
+private:
+    std::vector<Plant *> plants;
+};
+
+#endif
