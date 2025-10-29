9b2972dc6d40e2500a922bfff4d459519b55ce8c
Vele189 <6lordz@gmail.com>
2025-10-29 19:15:14 +0200

    some more logic added

--- Topiary.cpp
+++ Topiary.cpp
@@ -1,13 +1,18 @@
 #include "Topiary.h"
-#include "Plant.h" 
+#include "Plant.h"
+#include <iostream>
+
 void Topiary::water(Plant* plant) {
+    std::cout << "Watering Topiary plant '" << plant->getSpecies() << "' sparingly." << std::endl;
     plant->water();
 }
 
 void Topiary::prune(Plant* plant) {
+    std::cout << "Pruning Topiary plant '" << plant->getSpecies() << "' for specific shape." << std::endl;
     plant->prune();
 }
 
 void Topiary::fertilise(Plant* plant) {
+    std::cout << "Fertilising Topiary plant '" << plant->getSpecies() << "' with balanced feed." << std::endl;
     plant->fertilise();
 }
