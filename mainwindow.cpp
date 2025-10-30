9b9c98b4bc930215d48fd35cc7f2088b18e08fd3
Mulondi Makhado <makhadomulondi@gmail.com>
2025-10-30 12:21:39 +0200

    Added the Dashboard form

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "dashboard.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,12 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_startButton_clicked()
+{
+    // Option 2: Create new window and hide current window
+    Dashboard* dashboardWindow = new Dashboard();
+    dashboardWindow->show();
+    this->hide(); // Hide the main window
+}
+
